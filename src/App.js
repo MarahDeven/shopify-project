cbdf3a39e245a2a9588f22ea188ad0ffef965c7e
MarahDeven <marahdeven@gmail.com>
2022-05-10 11:54:43 -0400

    set up OpenAI component and created initial form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import "./App.css";
 import logo from "./logo.jpg";
-
+import OpenAI from "./OpenAI";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid rounded" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid rounded" alt="logo" />
+        </header>
+        <main>
+          <OpenAI />
+        </main>
+        <footer className="App-footer">
+          <small>
+            Coded by{" "}
+            <a
+              href="www.linkedin.com/in/marah-deven-rice"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Marah Rice
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://github.com/MarahDeven/shopify-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced
+            </a>{" "}
+            on Github
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
